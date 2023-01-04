a6d9d9fdc56c3c272a96671a38b85d37851048db
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-04 14:52:56 +0300

    add app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { RequestsCardComponent } from './admin/requests-card/requests-card.component';
+import { AddSectorComponent } from './admin/sectors-table/add-sector/add-sector.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 
 const routes: Routes = [
-  {path:'login',component:LoginFormComponent}
+  {path:'login',component:LoginFormComponent},
+  {path:'dashboard',component:DashboardComponent } ,
+  {path:'addSector',component:AddSectorComponent}
+  
+  
 ];
 
 @NgModule({
