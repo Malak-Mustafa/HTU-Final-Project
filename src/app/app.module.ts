1120c7ca1c57fd1dc178e03ca6d03f6f445ce6ad
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-04 14:53:50 +0300

    import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { MaterialModule } from './shared/material/material.module';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { SectorsTableComponent } from './admin/sectors-table/sectors-table.component';
+import { StartupsTableComponent } from './admin/startups-table/startups-table.component';
+import { RequestsCardComponent } from './admin/requests-card/requests-card.component';
+import { AddSectorComponent } from './admin/sectors-table/add-sector/add-sector.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    DashboardComponent,
+    SectorsTableComponent,
+    StartupsTableComponent,
+    RequestsCardComponent,
+    AddSectorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
