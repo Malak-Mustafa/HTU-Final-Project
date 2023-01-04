397bd404e82ba93f5c1662b53714808a594649a3
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-04 14:45:13 +0300

    add material module

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-const Materials =[MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
-  MatButtonModule]
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+const Materials = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatTabsModule,
+  MatTableModule,
+  MatIconModule,
+  MatDividerModule
+];
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    Materials
-  ],
-  exports:[
-    Materials
-  ]
+  imports: [CommonModule, Materials],
+  exports: [Materials],
 })
-export class MaterialModule { }
+export class MaterialModule {}
