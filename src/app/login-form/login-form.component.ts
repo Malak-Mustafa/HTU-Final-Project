bf3c6229d2394b708a6f96b23c096d9be632bcee
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-04 14:45:48 +0300

    add login form

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,13 +1,25 @@
+import { formatCurrency } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { admin } from './admin';
+import{ AdminService }from './admin.service'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent {
-  loginForm(form:any){
-    console.log(form);
+  constructor(private AdminService:AdminService, private router:Router){
+
+  }
+ 
+  login(form:any){
+    let admin: admin={
+    email: form.value.email,
+    password: form.value.password,
     
+  };
+  //this.AdminService.Auth(admin);
+  this.router.navigate(['dashboard/']);
   }
 }
