add28582e8877197095b2d3de79cc3d99643ff3d
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-04 14:57:08 +0300

    delete json service file

--- src/app/login-form/admin.service.ts
+++ src/app/login-form/admin.service.ts
@@ -7,17 +7,5 @@ import { admin } from './admin';
   providedIn: 'root',
 })
 export class AdminService {
-  baseUrl: string = 'http://localhost:3000/admins';
-  admins: admin[] = [];
-  constructor(private http: HttpClient) {}
-
-  getAdmins(): Observable<admin[]> {
-    return this.http.get<admin[]>(this.baseUrl);
-  }
-
-  Auth(admin: admin) {
-    //this.admins.push(admin);
-    //console.log(this.admins);
-    
-  }
+ 
 }
