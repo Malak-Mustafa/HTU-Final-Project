df49447a4fee64cb1126fd20e237ae73e7180848
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-05 15:05:01 +0300

    add homepage& auth paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { RequestsCardComponent } from './admin/requests-card/requests-card.component';
 import { AddSectorComponent } from './admin/sectors-table/add-sector/add-sector.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 
 const routes: Routes = [
-  {path:'login',component:LoginFormComponent},
-  {path:'dashboard',component:DashboardComponent } ,
-  {path:'addSector',component:AddSectorComponent}
-  
+  // {path:'login',component:LoginFormComponent},
+  // {path:'dashboard',component:DashboardComponent } ,
+  // {path:'addSector',component:AddSectorComponent},
+  {path:'', component:HomePageComponent,pathMatch:'full'},
+  {path: 'auth', loadChildren:()=>import('./auth/auth-routing.module').then((m)=>m.AuthRoutingModule)}
   
 ];
 
