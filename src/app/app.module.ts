90794724ce47cb21ee6513ab903a79aab8bece70
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-05 11:17:09 +0300

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import { SectorsTableComponent } from './admin/sectors-table/sectors-table.compo
 import { StartupsTableComponent } from './admin/startups-table/startups-table.component';
 import { RequestsCardComponent } from './admin/requests-card/requests-card.component';
 import { AddSectorComponent } from './admin/sectors-table/add-sector/add-sector.component';
-
+import { AngularFireModule} from '@angular/fire/compat';
+import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +31,17 @@ import { AddSectorComponent } from './admin/sectors-table/add-sector/add-sector.
     MaterialModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAAbpTma4mmz-imbM3uNQbMATf72adwrHY",
+      authDomain: "capstonproject-772f0.firebaseapp.com",
+      projectId: "capstonproject-772f0",
+      storageBucket: "capstonproject-772f0.appspot.com",
+      messagingSenderId: "536078032156",
+      appId: "1:536078032156:web:8b541e8344ab7455b02ed9",
+      measurementId: "G-Q6E4SRLH3R"
+    }),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
