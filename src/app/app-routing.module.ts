06f5cb992644947d3080f154069b5cce8681c0ed
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-06 12:41:46 +0300

    edit lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { RequestsCardComponent } from './admin/requests-card/requests-card.component';
 import { AddSectorComponent } from './admin/sectors-table/add-sector/add-sector.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+
 
 const routes: Routes = [
   // {path:'login',component:LoginFormComponent},
   // {path:'dashboard',component:DashboardComponent } ,
   // {path:'addSector',component:AddSectorComponent},
   {path:'', component:HomePageComponent,pathMatch:'full'},
-  {path: 'auth', loadChildren:()=>import('./auth/auth-routing.module').then((m)=>m.AuthRoutingModule)}
+  {path: 'auth', loadChildren:()=>import('./auth/auth.module').then((m)=>m.AuthModule)}
   
 ];
 
