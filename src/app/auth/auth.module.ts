e884d02f7cfb389fd22fa8de3fbcfbb659c876c1
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-06 12:42:26 +0300

    add material & form modules

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { MaterialModule } from '../shared/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
