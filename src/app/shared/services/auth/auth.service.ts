a68c9b5de7a76824cb41c9601c582c44a34af2c0
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-07 12:07:56 +0300

    add user state Observable

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { from } from 'rxjs';
+import { from,Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
+ userState$= this.FireAuth.authState;
 
-  constructor(private FireAuth:AngularFireAuth) { }
+  constructor(private FireAuth:AngularFireAuth) {}
 
   signIn(email:string,password:string){
   return this.FireAuth.signInWithEmailAndPassword(email,password);
