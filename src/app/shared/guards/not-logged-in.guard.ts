d44d760034950f8cccc061ac97bdacb1cf999dc9
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-07 13:14:53 +0300

    define guard jop

--- src/app/shared/guards/not-logged-in.guard.ts
+++ src/app/shared/guards/not-logged-in.guard.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AuthService } from '../services/auth/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotLoggedInGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+
+    let guardObsv = this.authService.userState$.pipe(
+      map((value) => {
+        if (!value) return true;
+        else {
+          //navigate to admin
+          this.router.navigate(['admin/']);
+
+          return false;
+        }
+      })
+    );
+    return guardObsv;
   }
-  
 }
