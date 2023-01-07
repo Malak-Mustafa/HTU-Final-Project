8342f57ef0737c256a53e9327dc51a00b9cbc932
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-07 13:16:42 +0300

    add NotLoggedInGuard and clear unUsed imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './admin/dashboard/dashboard.component';
-import { RequestsCardComponent } from './admin/requests-card/requests-card.component';
-import { AddSectorComponent } from './admin/sectors-table/add-sector/add-sector.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AuthGuard } from './shared/guards/auth.guard';
+import { NotLoggedInGuard } from './shared/guards/not-logged-in.guard';
 
 
 const routes: Routes = [
-  // {path:'login',component:LoginFormComponent},
-  // {path:'dashboard',component:DashboardComponent } ,
-  // {path:'addSector',component:AddSectorComponent},
   {path:'', component:HomePageComponent,pathMatch:'full'},
-  {path: 'auth', loadChildren:()=>import('./auth/auth.module').then((m)=>m.AuthModule)},
+  {path: 'auth', loadChildren:()=>import('./auth/auth.module').then((m)=>m.AuthModule),
+canActivate:[NotLoggedInGuard]},
   {path: 'admin', loadChildren:()=>import('./admin/admin.module').then((m)=>m.AdminModule),
   canActivate:[AuthGuard]}
   
