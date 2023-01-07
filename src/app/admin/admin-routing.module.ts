2a7d432bf03ecab0316934f4403f1f114f1dff7e
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-07 14:32:50 +0300

    remove add sector component to sectors module

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AddSectorComponent } from './sectors-table/add-sector/add-sector.component';
+
 
 const routes: Routes = [
   {path:'dashboard',component:DashboardComponent } ,
-  {path:'addSector',component:AddSectorComponent},
+ 
 ];
 
 @NgModule({
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,7 +6,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { SectorsTableComponent } from './sectors-table/sectors-table.component';
 import { StartupsTableComponent } from './startups-table/startups-table.component';
 import { RequestsCardComponent } from './requests-card/requests-card.component';
-import { AddSectorComponent } from './sectors-table/add-sector/add-sector.component';
 import { MaterialModule } from '../shared/material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -16,7 +15,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     SectorsTableComponent,
     StartupsTableComponent,
     RequestsCardComponent,
-    AddSectorComponent,
   ],
   imports: [
     CommonModule,
