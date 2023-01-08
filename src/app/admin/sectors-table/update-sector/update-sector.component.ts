8c90f66a3909d400cf8f2dfb7337c0ffb1e915be
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-08 15:00:21 +0300

    add main elements of the form and some style

--- src/app/admin/sectors-table/update-sector/update-sector.component.ts
+++ src/app/admin/sectors-table/update-sector/update-sector.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { of, switchMap } from 'rxjs';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
+import { SectorsFirebaseServiceService } from 'src/app/shared/services/storege/sectors-firebase-service.service';
 @Component({
   selector: 'app-update-sector',
   templateUrl: './update-sector.component.html',
   styleUrls: ['./update-sector.component.css']
 })
 export class UpdateSectorComponent {
+  updateSectorForm= new FormGroup({
+    sectorName :new FormControl('',[Validators.required]),
+    sectorLogo: new FormControl('',[Validators.required]),
+    sectorDesignColor: new FormControl('',[Validators.required]),
+  
+  });
+  
+  get sectorName(){
+    return this.updateSectorForm.get('sectorName')
+  }
+  get sectorLogo(){
+    return this.updateSectorForm.get('sectorLogo')
+  }
+  get sectorDesignColor(){
+    return this.updateSectorForm.get('sectorDesignColor')
+  }
 
+  onSubmit(){}
 }
