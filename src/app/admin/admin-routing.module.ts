56b625ae2248fab86548e0ea5d8b296409f11884
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-09 12:58:16 +0300

    add startup/addStartup route

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -5,7 +5,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path:'dashboard',component:DashboardComponent } ,
-  {path: 'sector', loadChildren:()=>import('./sectors-table/sectors.module').then((m)=>m.SectorsModule),}
+  {path: 'sector', loadChildren:()=>import('./sectors-table/sectors.module').then((m)=>m.SectorsModule),},
+  {path: 'startup', loadChildren:()=>import('./startups-table/startups.module').then((m)=>m.StartupsModule),}
 ];
 
 @NgModule({
