2a9663a99a04302fe2652f23dec27b51f4c7cd64
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-09 11:31:46 +0300

    add img storage

--- src/app/admin/sectors-table/add-sector/add-sector.component.ts
+++ src/app/admin/sectors-table/add-sector/add-sector.component.ts
@@ -3,55 +3,71 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { of, switchMap } from 'rxjs';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
+import { FireStorageService } from 'src/app/shared/services/storege/fire-storage.service';
 import { SectorsFirebaseServiceService } from 'src/app/shared/services/storege/sectors-firebase-service.service';
 
 @Component({
   selector: 'app-add-sector',
   templateUrl: './add-sector.component.html',
-  styleUrls: ['./add-sector.component.css']
+  styleUrls: ['./add-sector.component.css'],
 })
 export class AddSectorComponent {
-constructor(private router:Router,private authService: AuthService , private sectorService:SectorsFirebaseServiceService){}
-addSectorForm= new FormGroup({
-  sectorName :new FormControl('',[Validators.required]),
-  sectorLogo: new FormControl('',[Validators.required]),
-  sectorDesignColor: new FormControl('',[Validators.required]),
+  downloadUrl? :string;
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private sectorService: SectorsFirebaseServiceService,
+    private storage: FireStorageService
+  ) {}
+  addSectorForm = new FormGroup({
+    sectorName: new FormControl('', [Validators.required]),
+    // sectorLogo: new FormControl('',[Validators.required]),
+    sectorDesignColor: new FormControl('', [Validators.required]),
+  });
 
-});
-
-get sectorName(){
-  return this.addSectorForm.get('sectorName')
-}
-get sectorLogo(){
-  return this.addSectorForm.get('sectorLogo')
-}
-get sectorDesignColor(){
-  return this.addSectorForm.get('sectorDesignColor')
-}
-onSubmit(){
-
-  this.authService.userState$
-  .pipe(
-      switchMap((value) => {
-        
-        if(value){
-         return this.sectorService.addSector({
-            SectorName:this.sectorName?.value+'',
-            SectorLogo:this.sectorLogo?.value+'',
-            sectorDesignColor:this.sectorDesignColor?.value+'',
-            userId:value.uid,
-          })
-        }
-          else {
+  get sectorName() {
+    return this.addSectorForm.get('sectorName');
+  }
+  // get sectorLogo(){
+  //   return this.addSectorForm.get('sectorLogo')
+  // }
+  get sectorDesignColor() {
+    return this.addSectorForm.get('sectorDesignColor');
+  }
+  onSubmit() {
+    this.authService.userState$
+      .pipe(
+        switchMap((value) => {
+          if (value) {
+            return this.sectorService.addSector({
+              SectorName: this.sectorName?.value + '',
+              sectorDesignColor: this.sectorDesignColor?.value + '',
+              userId: value.uid,
+              SectorLogo:this.downloadUrl+ '',
+              
+            });
+          } else {
             return of(null);
           }
-      })
-  ).subscribe((val)=> {
-    if(!val){
-      alert('cannot add sector');
-    }
-  })
-  this.router.navigate(['/admin/dashboard']);
-}
+        })
+      )
+      .subscribe((val) => {
+        if (!val) {
+          alert('cannot add sector');
+        }
+      });
+    this.router.navigate(['/admin/dashboard']);
+  }
 
+  upload(event: Event) {
+    console.log(event);
+    let file = (event.target as HTMLInputElement)?.files?.[0];
+    if(file){
+      this.storage.uploadFile(file).subscribe((val)=>{
+        this.downloadUrl=val;
+        
+      })
+    }
+    
+  }
 }
