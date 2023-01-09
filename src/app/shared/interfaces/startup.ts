04743828e038cd8a82c888a6844b44ffb1ac10a9
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-09 12:59:10 +0300

    create startup interface

---
+++ src/app/shared/interfaces/startup.ts
@@ -0,0 +1,13 @@
+export interface startup{
+  StartupName: string;
+  id?: number;
+  userId: string,
+  StartupLogo: string;
+  StartupCity: string;
+  Sectors: string;
+  FounderName: string;
+  NumberOfEmployees: number;
+  YearOfEstablishment: number;
+  URL: string;
+  EmailAddress: string;
+}
