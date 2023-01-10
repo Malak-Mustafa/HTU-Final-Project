fe2c7dc54ecdac3feab387b15603f38d32a9d172
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-10 22:23:16 +0300

    create request interface

---
+++ src/app/shared/interfaces/request.ts
@@ -0,0 +1,12 @@
+export interface request{
+    StartupName: string;
+    id?: string;
+    StartupLogo: string;
+    StartupCity: string;
+    SectorID: string;
+    FounderName: string;
+    NumberOfEmployees: string;
+    YearOfEstablishment: string;
+    URL: string;
+    EmailAddress: string;
+  }
