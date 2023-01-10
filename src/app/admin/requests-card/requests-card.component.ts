84f5258cf8c75f3c1d47fc4387c20e58833385ee
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-10 22:26:30 +0300

    edit card data

--- src/app/admin/requests-card/requests-card.component.ts
+++ src/app/admin/requests-card/requests-card.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardContent } from '@angular/material/card';
+import { of, switchMap } from 'rxjs';
+import { request } from 'src/app/shared/interfaces/request';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
+import { RequestsFirebaseService } from 'src/app/shared/services/storege/requests-firebase.service';
+import { SectorsFirebaseServiceService } from 'src/app/shared/services/storege/sectors-firebase-service.service';
 @Component({
   selector: 'app-requests-card',
   templateUrl: './requests-card.component.html',
@@ -7,52 +13,83 @@ import { Component } from '@angular/core';
 })
 
 export class RequestsCardComponent {
-  requests: requestInfo[] = [
-    {
-      StartupName: 'test',
-      StartupLogo:
-        'https://cdn.shopify.com/shopifycloud/hatchful_web_two/bundles/4a14e7b2de7f6eaf5a6c98cb8c00b8de.png',
-      StartupCity: 'Amman',
-      Sectors: 'IT',
-      FounderName: 'abood',
-      NumberOfEmployees: 100,
-      YearOfEstablishment: 2000,
-      URL: 'https//abood',
-      EmailAddress: 'abood@abood',
-    }, {
-     
-      StartupName: 'test2',
-      StartupLogo:
-        'https://cdn.shopify.com/shopifycloud/hatchful_web_two/bundles/4a14e7b2de7f6eaf5a6c98cb8c00b8de.png',
-      StartupCity: 'Amman',
-      Sectors: 'IT',
-      FounderName: 'abood',
-      NumberOfEmployees: 100,
-      YearOfEstablishment: 2000,
-      URL: 'https//abood',
-      EmailAddress: 'abood@abood',
-    },
-  ];
-constructor(){}
+  requests?: request[] 
+  SectorsData?: any;
+  SectorName?: string;
+constructor(private authService: AuthService, 
+private sectorsService: SectorsFirebaseServiceService,
+private requestsService: RequestsFirebaseService,){}
+ngOnInit(): void {
+  this.authService.userState$
+    .pipe(
+      switchMap((val) => {
+        if (val) {
+          return this.requestsService.getRequests();
+        } else {
+          return of(null);
+        }
+      })
+    )
+    .subscribe((response) => {
+      if (response) {
+        this.requests = response;
+        this.getSectorsData(response)
+        console.log(this.requests);
+        
+      }
+    });
+}
+
+
+getSectorsData(data:any){
+  this.SectorsData = {}
+  for (let i = 0; i < data.length; i++) {
+    // @ts-ignore: Unreachable code error
+    this.SectorsData[data[i].SectorID] = ""
+  }
+  for (const id in this.SectorsData) {
+    this.authService.userState$
+      .pipe(
+        switchMap((val) => {
+          if (val) {
+            return this.sectorsService.getSector(id);
+          } else {
+            console.log( 'cannot get sector');
+            
+            return of(null);
+          }
+        })
+      )
+      .subscribe((response) => {
+        if (response) {
+          // @ts-ignore: Unreachable code error
+          this.SectorsData[id] = response.SectorName
+        }
+      });
+  }
+}
 addStartup(){
   console.log("add");
   
 }
-deleteRequest(){
-  console.log("delete");
+deleteRequest(id:string){
+  this.authService.userState$
+      .pipe(
+        switchMap((value) => {
+          if (value) {
+            return this.requestsService.deleteRequest(id);
+          } else {
+            return of(null);
+          }
+        })
+      )
+      .subscribe((val) => {
+        if (!val) {
+          alert('request deleted!');
+        }
+      });
   
 }
 }
- export interface requestInfo{
-  StartupName: string;
-  StartupLogo: string;
-  StartupCity: string;
-  Sectors: string;
-  FounderName: string;
-  NumberOfEmployees: number;
-  YearOfEstablishment: number;
-  URL: string;
-  EmailAddress: string;
- }
-  
+ 
 
