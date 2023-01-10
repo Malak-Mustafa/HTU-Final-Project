c1cde62b9c59bccbcea8acd6baa508ac37518fbe
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-10 15:29:36 +0300

    create startup update component

--- src/app/admin/startups-table/startups.module.ts
+++ src/app/admin/startups-table/startups.module.ts
@@ -5,11 +5,13 @@ import { StartupsRoutingModule } from './startups-routing.module';
 import { AddStartupComponent } from './add-startup/add-startup.component';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UpdateStartupComponent } from './update-startup/update-startup.component';
 
 
 @NgModule({
   declarations: [
-    AddStartupComponent
+    AddStartupComponent,
+    UpdateStartupComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/admin/startups-table/update-startup/update-startup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-startup',
+  templateUrl: './update-startup.component.html',
+  styleUrls: ['./update-startup.component.css']
+})
+export class UpdateStartupComponent {
+
+}
