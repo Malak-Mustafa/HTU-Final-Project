d3a4e8f70251ce94115d097f00ef57609a10452c
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-10 15:41:48 +0300

    set the routs

--- src/app/admin/startups-table/startups-routing.module.ts
+++ src/app/admin/startups-table/startups-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddStartupComponent } from './add-startup/add-startup.component';
+import { UpdateStartupComponent } from './update-startup/update-startup.component';
 
 const routes: Routes = [
   {path:'addStartup',component:AddStartupComponent},
+  {path:'updateStartup',component:UpdateStartupComponent},
 ];
 
 @NgModule({
