f1b5c9a6f709d333a92eb86bc312e08898de3a67
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-20 21:42:06 +0300

    add startup dialog

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { of, switchMap,Observable } from 'rxjs';
 import { sector } from '../shared/interfaces/sector';
 import { FireStorageService } from '../shared/services/storege/fire-storage.service';
 import { SectorsFirebaseServiceService } from '../shared/services/storege/sectors-firebase-service.service';
 import { StartupFirebaseService } from '../shared/services/storege/startup-firebase.service';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-home-page',
@@ -19,7 +20,9 @@ export class HomePageComponent {
   constructor( private router: Router,
     private startupsService: StartupFirebaseService,
     private storage: FireStorageService,
-    private sectorsService: SectorsFirebaseServiceService){
+    private sectorsService: SectorsFirebaseServiceService,
+    public dialog: MatDialog
+    ){
   }
   ngOnInit(): void {
     this.startupsService.getStartups().subscribe((response) => {
@@ -55,5 +58,31 @@ export class HomePageComponent {
       const startup = this.dataSource[i]
       this.SectorsData[startup.SectorID].startups.push(startup)
     }
+  }
+  openDialog(data:any): void {
+    const dialogRef = this.dialog.open(StartupDialog, {
+      data,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+}
+
+// =================
+@Component({
+  selector: 'startupDialog',
+  templateUrl: 'startupDialog.html',
+  styleUrls: ['./startupDialog.css']
+})
+export class StartupDialog {
+  constructor(
+    public dialogRef: MatDialogRef<StartupDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
