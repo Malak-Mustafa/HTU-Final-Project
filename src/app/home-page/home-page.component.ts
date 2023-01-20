7cae4ff9b08abf7b88b5d364b6d06857f92647bb
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-20 18:02:32 +0300

    get data on home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { of, switchMap,Observable } from 'rxjs';
+import { sector } from '../shared/interfaces/sector';
+import { FireStorageService } from '../shared/services/storege/fire-storage.service';
+import { SectorsFirebaseServiceService } from '../shared/services/storege/sectors-firebase-service.service';
+import { StartupFirebaseService } from '../shared/services/storege/startup-firebase.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +12,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
-
+  objectKeys = Object.keys;
+  dataSource?:any;
+  SectorsData?: any;
+  // SectorName?: string;
+  constructor( private router: Router,
+    private startupsService: StartupFirebaseService,
+    private storage: FireStorageService,
+    private sectorsService: SectorsFirebaseServiceService){
+  }
+  ngOnInit(): void {
+    this.startupsService.getStartups().subscribe((response) => {
+      if (response) {
+        this.dataSource= response;
+        this.getSectorsData(response)
+      }
+    });  
+  }
+  getSectorsData(data:any){
+    this.SectorsData = {}
+    for (let i = 0; i < data.length; i++) {
+      // @ts-ignore: Unreachable code error
+      this.SectorsData[data[i].SectorID] = ""
+    }
+    const IDs = Object.keys(this.SectorsData)
+    for (const id in this.SectorsData) {
+      this.sectorsService.getSector(id)
+      .subscribe((response) => {
+        if (response) {
+          // @ts-ignore: Unreachable code error
+          this.SectorsData[id] = {...response, startups:[]}
+        }
+        // run this on last element
+        if (IDs[IDs.length-1] == id) {
+          this.setData()
+        }
+      });
+    }
+  }
+  setData(){
+    for (let i = 0; i < this.dataSource.length; i++) {
+      const startup = this.dataSource[i]
+      this.SectorsData[startup.SectorID].startups.push(startup)
+    }
+  }
 }
