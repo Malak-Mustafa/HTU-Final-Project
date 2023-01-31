55bf306ff50838a68e65ff396eede4f0768b5a60
Malak-Mustafa <malak2000moayad@gmail.com>
2023-01-31 19:47:48 +0300

    delete unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule} from '@angular/fire/compat';
 import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
 import { HomePageComponent } from './home-page/home-page.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { RequestFormComponent } from './home-page/request-form/request-form.component';
-import { SectorContainerComponent } from './home-page/sector-container/sector-container.component';
-import { StartupCardComponent } from './home-page/startup-card/startup-card.component';
 import { StartupDetailsComponent } from './home-page/startup-details/startup-details.component';
 
 
@@ -26,8 +22,6 @@ import { StartupDetailsComponent } from './home-page/startup-details/startup-det
     AppComponent,
     HomePageComponent,
     RequestFormComponent,
-    SectorContainerComponent,
-    StartupCardComponent,
     StartupDetailsComponent,
     
   ],
